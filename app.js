cec2bc0420f15fae141bf506dd48078350a9e084
Ayush Dangol <ayushdangol47@gmail.com>
2022-06-29 14:17:58 +0545

    configure github actions

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express()
+const path = require('path')
 
 
+app.use(express.static(path.join(__dirname, '/frontned/build')));
 app.get('/names', (req, res) => {
     res.send('Hello');
 })
