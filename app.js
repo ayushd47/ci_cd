5edb7a331241124b320acc330843cba25016e17e
Ayush Dangol <ayushdangol47@gmail.com>
2022-06-29 16:01:18 +0545

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, '/frontned/build')));
 app.get('/', (req, res) => {
-    res.send('Hello Ayush');
+    res.send('Bye Ayush');
 })
 
 app.listen(5000, () => {
